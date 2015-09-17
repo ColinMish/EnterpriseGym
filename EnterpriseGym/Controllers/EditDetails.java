d1ea3492054bff49b3211ee23a0a8116e82d3565
unknown <kristiyangeorgiev@labpc442.computing.dundee.ac.uk>
2015-09-17 14:54:02 +0100

    Edit Details page added

---
+++ EnterpriseGym/Controllers/EditDetails.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ *
+ * @author kristiyangeorgiev
+ */
+@WebServlet(name = "EditDetails", urlPatterns = {"/EditDetails"})
+@MultipartConfig
+public class EditDetails extends HttpServlet {
+
+    /**
+     * Constructor
+     */
+    public EditDetails() {
+
+    }
+
+    /**
+     *
+     * @param config
+     * @throws ServletException
+     */
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+    }
+
+    /**
+     * Method to get the account details for display
+     *
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+    {
+                  RequestDispatcher dispatcher = request.getRequestDispatcher("editdetails.jsp");
+                dispatcher.forward(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException 
+    {
+    }
+}
