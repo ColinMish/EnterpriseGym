9306ca4423f9a97bcb8571b737108209ced56eb6
David <dkenny@dundee.ac.uk>
2015-09-17 10:35:29 +0100

    Working on registration
    Further work being done to the registration model.

--- EnterpriseGym/Controllers/Models/UserModel.java
+++ EnterpriseGym/Controllers/Models/UserModel.java
@@ -8,6 +8,7 @@ package Models;
 
 import java.sql.*;
 import javax.servlet.http.HttpSession;
+import java.util.Date;
 
 /**
  *
@@ -32,32 +33,37 @@ public class UserModel {
 		
 	    Class.forName("com.mysql.jdbc.Driver").newInstance();
 	    con = DriverManager.getConnection("jdbc:mysql://160.153.16.42:3306/Enterprise_Gym",user,pass);
+            
+            Date date = new Date();
 	    
 	    PreparedStatement ps = null;
             PreparedStatement ps2 = null;
+            
+            String sqlOption2= "INSERT INTO account (username,password,date_joined) VALUES (?,?,?)";
+            ps2 = con.prepareStatement(sqlOption2);
+            ps2.setString(1, username);
+            ps2.setString(2, password);
+            ps2.setDate(3, date);
+            ps2.executeUpdate();
 	    
 	    
 	    String sqlOption= "INSERT INTO user (email,first_name,last_name,gender,country,university,school,subject,year,matriculation) VALUES (?,?,?,?,?,?,?,?,?,?)";
     	
-	ps = con.prepareStatement(sqlOption);
+            ps = con.prepareStatement(sqlOption);
  
-    	ps.setString(1, email);
-        ps.setString(2, first);
-        ps.setString(3, last);
-        ps.setString(4, gender);
-        ps.setString(5, country);
-        ps.setString(6, university);
-        ps.setString(7, school);
-        ps.setString(8, subject);
-        ps.setString(9, year);
-        ps.setString(10,matriculation);
-    	ps.executeUpdate();
+            ps.setString(1, email);
+            ps.setString(2, first);
+            ps.setString(3, last);
+            ps.setString(4, gender);
+            ps.setString(5, country);
+            ps.setString(6, university);
+            ps.setString(7, school);
+            ps.setString(8, subject);
+            ps.setString(9, year);
+            ps.setString(10,matriculation);
+            ps.executeUpdate();
         
-        String sqlOption2= "INSERT INTO account (username,password) VALUES (?,?)";
-        ps2 = con.prepareStatement(sqlOption2);
-        ps2.setString(1, username);
-        ps2.setString(2, password);
-        ps2.executeUpdate();
+        
         
         con.close();
     	
@@ -97,7 +103,7 @@ public class UserModel {
 	    PreparedStatement psdoLogin = null;
 
 	    	    
-	    	String sqlOption= "SELECT * FROM user_profile where username=? and password=?";
+	    	String sqlOption= "SELECT * FROM account where username=? and password=?";
 	    	
 	    	psdoLogin = con.prepareStatement(sqlOption);
 	    	psdoLogin.setString(1, username);
