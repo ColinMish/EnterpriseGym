d31d51a7a9637291acae30567772400bca2d7d3f
kLundahl <kim.lundahl1@gmail.com>
2015-09-23 14:36:02 +0100

    Updated sql statements

--- Resources/AddIndividualToken.sql
+++ Resources/AddIndividualToken.sql
@@ -1,36 +1,36 @@
-CREATE PROCEDURE `AddIndividualTokens` ()
+CREATE DEFINER=`davidkenny`@`134.36.154.%` PROCEDURE `AddIndividualTokens`()
 BEGIN
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 1) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 1) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value (1, 'Can manage quizes');
     END IF;
     
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 2) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 2) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value (2, 'Can manage news itemns');
     END IF;
     
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 3) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 3) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value (3, 'Can manage event items');
     END IF;
     
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 4) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 4) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value  (4, 'Can ok participan attendance for an event');
     END IF;
     
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 5) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 5) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value (5, 'Can access charts');
     END IF;
     
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 6) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 6) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value (6, 'Can give out token');
     END IF;
     
-	IF (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 7) = null THEN 
+	IF NOT EXISTS (SELECT idaccessToken FROM accessToken WHERE idaccessToken = 7) THEN 
     INSERT INTO accessToken 
 	(idaccessToken, description) value (7, 'Can manage theme scores');
     END IF;
