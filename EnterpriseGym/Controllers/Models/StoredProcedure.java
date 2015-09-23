a8888ba359ee3dcf56f35c124017cc9f4ef06b9e
kLundahl <kim.lundahl1@gmail.com>
2015-09-23 11:55:58 +0100

    StoredProcedure java file added which can access the stored procedures

--- EnterpriseGym/Controllers/Models/StoredProcedure.java
+++ EnterpriseGym/Controllers/Models/StoredProcedure.java
@@ -1,46 +1,42 @@
-
 package Models;
-import static Models.UserModel.getCurrentDate;
-import java.sql.*;
 
+import java.sql.*;
 
 /**
  *
  * @author Kim
  */
 public class StoredProcedure {
-    
+
     public String user = "davidkenny";
     public String pass = "root1";
-    
-    public StoredProcedure(){
-        
+
+    public StoredProcedure() {
+
     }
-    
-    
-    public boolean isTableEmpty(String table, Connection c){
-        boolean isEmpty = true;
-        
-        
-        
-        return isEmpty;
-    }
-    
-    
-    public void runProcedure(){
-          Connection con = null;
+
+    public void runAddTheme() throws SQLException {
+        Connection con = null;
         try {
+
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con = DriverManager.getConnection("jdbc:mysql://160.153.16.42:3306/Enterprise_Gym", user, pass);
 
             PreparedStatement ps = null;
+            ResultSet rs = null;
+            String q = "SELECT count(*) FROM theme";
+            ps = con.prepareStatement(q);
+            rs = ps.executeQuery();
 
-            String InsertIntoAccount = "";
+            rs.next();
+            int rows = rs.getInt(1);
 
-            ps.executeUpdate();
-            
-        } catch (Exception e) {  }
+            if (rows == 0) {
+                /* Run  stored procedure */
+                PreparedStatement theme = con.prepareCall("{call SPAddThemes()}");
+                theme.execute();
+            }
+        } catch (Exception e) {
+        }
     }
-    
-    
 }
