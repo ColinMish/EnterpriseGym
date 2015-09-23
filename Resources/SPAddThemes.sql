84f3124b33b1e7c0d1f6be6c0c08751a837e4918
kLundahl <kim.lundahl1@gmail.com>
2015-09-23 14:06:17 +0100

    Mysql stored procedure query for adding themes into the database

--- Resources/SPAddThemes.sql
+++ Resources/SPAddThemes.sql
@@ -1,7 +1,10 @@
-CREATE PROCEDURE `AddThemes` ()
+CREATE DEFINER=`davidkenny`@`134.36.154.%` PROCEDURE `AddThemes`()
 BEGIN
-		insert into theme (idtheme, name, points) Value
-		((1, 'Action', 10),(2, 'Practie', 10), (3, 'Virtual', 10), 
+	IF (SELECT count(*) FROM theme)< 5 THEN DELETE FROM theme;
+    END IF;
+    
+	IF (SELECT count(*) FROM theme)= 0 THEN INSERT INTO theme (idtheme, name, points) Value
+		((1, 'Action', 10),(2, 'Practice', 10), (3, 'Virtual', 10), 
 		(4, 'Project', 10), (5, 'Theory', 10));
-
+        END IF;
 END
